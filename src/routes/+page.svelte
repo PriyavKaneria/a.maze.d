d4d3651b3f8ee6d4edfc835df2adc441c40a6545
PriyavKaneria <priyavkaneria@gmail.com>
2024-09-29 01:02:22 +0530

    Mobile responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { browser } from '$app/environment';
 	import { tweened } from 'svelte/motion';
 	import StonePaperScissors from '$lib/stone-paper-scissor.svg';
+	import StonePaperScissorsMobile from '$lib/stone-paper-scissor-mobile.svg';
 
 	let maze: Array<Array<number | 'stone' | 'paper' | 'scissors' | 'checkpoint'>> = [];
 	let playerX = tweened(25, { duration: 0 });
@@ -25,10 +26,32 @@
 	let showOverlay = false;
 	let overlayDiv: HTMLDivElement;
 
+	// popup form
+	let showPopup = false;
+	let playerName = '';
+	let playerLink = '';
+
 	const startTimer = () => {
 		timer = 0;
 		timerInterval = setInterval(() => {
 			timer++;
+			if (timer % 15 === 0) {
+				// spawn a random item at spawn
+				const items = ['stone', 'paper', 'scissors'];
+				const x = Math.floor(Math.random() * (MAZE_WIDTH - 2)) + 1;
+				const y = Math.floor(Math.random() * (EMPTY_ZONE_HEIGHT - 2 - 10)) + 10;
+				maze[y][x] = items[Math.floor(Math.random() * items.length)] as
+					| 'stone'
+					| 'paper'
+					| 'scissors'
+					| 'checkpoint';
+			}
+			if (timer === 180) {
+				alert('Random items spawn at start every 15s, if you get stuck');
+			}
+			if (timer === 300) {
+				alert('Use checkpoints efficiently to move around for collecting required items');
+			}
 		}, 1000);
 	};
 
@@ -107,7 +130,9 @@
 
 		// Add items
 		const items = ['stone', 'paper', 'scissors'];
-		for (let i = 0; i < 400; i++) {
+		const numberItems =
+			innerWidth > 1440 ? 400 : innerWidth > 1024 ? 300 : innerWidth > 768 ? 200 : 150;
+		for (let i = 0; i < numberItems; i++) {
 			const x = Math.floor(Math.random() * (MAZE_WIDTH - 2)) + 1;
 			const y =
 				Math.floor(Math.random() * (MAZE_HEIGHT - 2 * EMPTY_ZONE_HEIGHT - 2)) +
@@ -163,7 +188,15 @@
 	}
 
 	let lastMousePosition = 0;
-	function handleMouseMove(event: { clientX: number }) {
+	function handleMouseMove(event: MouseEvent | TouchEvent) {
+		// check touch input
+		if (event.type === 'touchmove') {
+			const touch = (event as TouchEvent).touches[0];
+			event = new MouseEvent('mousemove', {
+				clientX: touch.clientX
+			});
+		}
+		event = event as MouseEvent;
 		const targetCellX = Math.floor(event.clientX / CELL_SIZE);
 		lastMousePosition = event.clientX;
 
@@ -211,10 +244,10 @@
 				for (let y = $playerY - 1; y >= targetCellY; y--) {
 					if (checkCollision($playerX, y)) {
 						if (!freeRoam) {
-							window.scrollTo({
-								top: lastScrollPosition
-							});
-							break;
+							// window.scrollTo({
+							// 	top: lastScrollPosition
+							// });
+							// break;
 						}
 					}
 					playerY.set(y);
@@ -223,21 +256,45 @@
 				for (let y = $playerY + 1; y <= targetCellY; y++) {
 					if (checkCollision($playerX, y)) {
 						if (!freeRoam) {
-							window.scrollTo({
-								top: lastScrollPosition
-							});
-							break;
+							// window.scrollTo({
+							// 	top: lastScrollPosition
+							// });
+							// break;
 						}
 					}
 					playerY.set(y);
 				}
 			}
 			lastScrollPosition = window.scrollY;
-			handleMouseMove({ clientX: lastMousePosition });
+			const mouseEvent = new MouseEvent('mousemove', {
+				clientX: lastMousePosition
+			});
+			handleMouseMove(mouseEvent);
 		}
 	}
 
-	const handleMouseDown = (event: MouseEvent) => {
+	const handleMouseDown = (event: MouseEvent | TouchEvent) => {
+		// check touch input
+		if (event.type === 'touchstart') {
+			const touch = (event as TouchEvent).touches[0];
+
+			// double click to add checkpoint
+			if (touch.target === event.currentTarget) {
+				event.preventDefault();
+				if (inventory.checkpoints === 0) {
+					alert('You are out of checkpoints!');
+					return;
+				}
+				if (maze[$playerY][$playerX] === 'checkpoint') {
+					return;
+				}
+				maze[$playerY][$playerX] = 'checkpoint';
+				checkpoints.push({ x: $playerX, y: $playerY, scrollY: window.scrollY });
+				inventory.checkpoints--;
+			}
+		}
+
+		event = event as MouseEvent;
 		if (event.detail > 1) {
 			// double click stop text selection
 			event.preventDefault();
@@ -289,7 +346,24 @@
 				top: 0
 			});
 		}
+		return () => {
+			if (timerInterval) clearInterval(timerInterval);
+		};
 	});
+
+	const handleSubmit = async () => {
+		const response = await fetch('/api/leaderboard', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name: playerName, link: playerLink, time: timer })
+		});
+		if (response.ok) {
+			showPopup = false;
+			alert('Your score has been added to the leaderboard!');
+		} else {
+			alert('Failed to add your score. Please try again.');
+		}
+	};
 
 	// cheat detection - if player has not moves at every Y position between start and end, they are cheating
 	let maxY = EMPTY_ZONE_HEIGHT;
@@ -302,14 +376,13 @@
 			timerStarted = true;
 			startTimer();
 		}
-		// winning condition
+		// winnning condition
 		if (!freeRoam && value >= MAZE_HEIGHT - EMPTY_ZONE_HEIGHT - 1) {
-			// cheat detection
 			if (maxY < MAZE_HEIGHT - EMPTY_ZONE_HEIGHT - 1) {
 				alert('Cheating detected! You did not complete the maze!');
 				location.reload();
 			}
-			alert('Congratulations! You completed the maze in ' + timer + ' seconds!');
+			showPopup = true; // Show the popup form
 			clearInterval(timerInterval);
 			freeRoam = true;
 		}
@@ -340,86 +413,101 @@
 
 <div class="w-screen h-[350vh] pointer-events-none select-none">
 	<div
-		class="fixed top-0 left-0 w-full h-12 bg-white text-black flex items-center justify-center z-10 select-none gap-6"
+		class="fixed top-0 left-0 w-full h-32 md:h-12 bg-white text-sm md:text-lg text-black flex items-center justify-center z-10 select-none gap-6 flex-col md:flex-row overflow-visible"
 	>
-		<span>Inventory:</span>
-		<span class="flex gap-1">
-			{#if useEmojis}
-				🪨
-			{:else}
-				<span class="bg-red-500 w-5 h-5 flex justify-center items-center" />
-			{/if}
-			Stone : {inventory.stone}
-		</span>
-		<span class="flex gap-1">
-			{#if useEmojis}
-				📄
-			{:else}
-				<span class="bg-blue-500 w-5 h-5 flex justify-center items-center" />
-			{/if}
-			Paper : {inventory.paper}
-		</span>
-		<span class="flex gap-1">
-			{#if useEmojis}
-				✂️
-			{:else}
-				<span class="bg-green-500 w-5 h-5 flex justify-center items-center" />
-			{/if}
-			Scissors : {inventory.scissors}
-		</span>
-		<span class="flex gap-1">
-			{#if useEmojis}
-				🚩
-			{:else}
-				<span class="bg-yellow-500 w-5 h-5 flex justify-center items-center" />
-			{/if}
-			Checkpoints : {inventory.checkpoints}
-		</span>
-
-		<!-- radio button group for emojis/color -->
-		<div class="flex gap-2 ml-4 pointer-events-auto cursor-pointer z-20">
-			<label>
-				<input
-					type="radio"
-					name="useEmojis"
-					checked={useEmojis}
-					on:change={() => {
-						useEmojis = true;
-					}}
-				/>
-				use emojis
-			</label>
-			<label>
-				<input
-					type="radio"
-					name="useEmojis"
-					checked={!useEmojis}
-					on:change={() => {
-						useEmojis = false;
-					}}
-				/>
-				use colors
-			</label>
+		<div class="flex gap-6">
+			<span>Inventory:</span>
+			<div class="flex gap-6 flex-col md:flex-row">
+				<div class="flex gap-6">
+					<span class="flex gap-1">
+						{#if useEmojis}
+							🪨
+						{:else}
+							<span class="bg-red-500 w-5 h-5 flex justify-center items-center" />
+						{/if}
+						Stone : {inventory.stone}
+					</span>
+					<span class="flex gap-1">
+						{#if useEmojis}
+							📄
+						{:else}
+							<span class="bg-blue-500 w-5 h-5 flex justify-center items-center" />
+						{/if}
+						Paper : {inventory.paper}
+					</span>
+				</div>
+				<div class="flex gap-6">
+					<span class="flex gap-1">
+						{#if useEmojis}
+							✂️
+						{:else}
+							<span class="bg-green-500 w-5 h-5 flex justify-center items-center" />
+						{/if}
+						Scissors : {inventory.scissors}
+					</span>
+					<span class="flex gap-1">
+						{#if useEmojis}
+							🚩
+						{:else}
+							<span class="bg-yellow-500 w-5 h-5 flex justify-center items-center" />
+						{/if}
+						Checkpoints : {inventory.checkpoints}
+					</span>
+				</div>
+			</div>
 		</div>
-		{#if freeRoam}
-			<button
-				class="bg-red-500 text-white px-4 py-2 rounded-md pointer-events-auto cursor-pointer"
-				on:click={() => {
-					location.reload();
-				}}
+
+		<div class="flex items-center gap-4">
+			<!-- radio button group for emojis/color -->
+			<div class="flex gap-2 ml-4 pointer-events-auto cursor-pointer z-20">
+				<label>
+					<input
+						type="radio"
+						name="useEmojis"
+						checked={useEmojis}
+						on:change={() => {
+							useEmojis = true;
+						}}
+					/>
+					use emojis
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="useEmojis"
+						checked={!useEmojis}
+						on:change={() => {
+							useEmojis = false;
+						}}
+					/>
+					use colors
+				</label>
+			</div>
+			{#if freeRoam}
+				<button
+					class="bg-red-500 text-white px-4 py-2 rounded-md pointer-events-auto cursor-pointer"
+					on:click={() => {
+						location.reload();
+					}}
+				>
+					Play again
+				</button>
+			{:else if timerStarted}
+				<span class="md:text-lg">Time: {timer}s</span>
+				<button
+					class="bg-blue-500 text-white px-2 md:px-4 py-1 md:py-2 rounded-md pointer-events-auto cursor-pointer"
+					on:click={handleRestart}
+				>
+					Restart
+				</button>
+			{/if}
+			<a
+				class="bg-amber-500 text-white px-2 md:px-4 py-1 md:py-2 rounded-md pointer-events-auto cursor-pointer fixed top-32 md:top-12 right-6"
+				href="/leaderboard"
 			>
-				Play again
-			</button>
-		{:else if timerStarted}
-			<span class="text-lg">Time: {timer}s</span>
-			<button
-				class="bg-blue-500 text-white px-4 py-2 rounded-md pointer-events-auto cursor-pointer"
-				on:click={handleRestart}
-			>
-				Restart
-			</button>
-		{/if}
-		<!-- <button
+				Leaderboard
+			</a>
+			<!-- <button
 			class="bg-red-600 text-white px-4 py-2 rounded-md pointer-events-auto cursor-pointer"
 			on:click={() => {
 				showOverlay = true;
@@ -428,25 +516,36 @@
 		>
 			Hard mode
 		</button> -->
+		</div>
 	</div>
 	<div class="flex justify-center items-start">
 		<img
 			alt="Stone(red) replaces Scissors(blue) replaces Paper(green) replaces stone, click to place checkpoint, right click to teleport to last checkpoint"
-			src={StonePaperScissors}
-			class="absolute top-16 h-[35%] pointer-events-none select-none"
+			src={innerWidth > 768 ? StonePaperScissors : StonePaperScissorsMobile}
+			class="absolute top-16 mt-24 md:mt-6 h-[30%] md:h-[35%] pointer-events-none select-none"
 		/>
 	</div>
+	<!-- go to checkpoint button for mobile -->
+	{#if checkpoints.length > 0}
+		<button
+			class="fixed bottom-0 right-0 m-4 bg-yellow-400 text-black p-2 rounded-md pointer-events-auto cursor-pointer z-10"
+			on:click={handleRightClick}
+		>
+			🚩
+		</button>
+	{/if}
 	<div
-		class="absolute top-0 left-0 w-screen h-[350vh] pointer-events-auto"
+		class="absolute top-0 left-0 w-screen h-[350vh] pointer-events-auto overflow-hidden"
 		aria-hidden="true"
 		on:mousedown={handleMouseDown}
+		on:touchmove={handleMouseMove}
 		on:contextmenu={handleRightClick}
 	>
 		{#each maze as row, y}
 			<div class="flex">
 				{#each row as cell, x}
 					<div
-						class="w-5 h-5 text-center items-center justify-center flex overflow-visible"
+						class="w-5 h-5 flex-shrink-0 text-center items-center justify-center flex overflow-visible"
 						class:bg-gray-800={cell === 1}
 						class:bg-red-500={cell === 'stone' && !useEmojis}
 						class:bg-blue-500={cell === 'paper' && !useEmojis}
@@ -479,3 +578,44 @@
 <!-- Only a circular radius of maze is visible -->
 <!-- <div class="fixed top-0 left-0 w-screen h-screen bg-black z-10" bind:this={overlayDiv}></div>
 {/if} -->
+
+{#if showPopup}
+	<div class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h2 class="text-xl font-bold mb-4">
+				Congratulations! You completed the maze in {timer} seconds!
+			</h2>
+			<form on:submit|preventDefault={handleSubmit}>
+				<div class="mb-4">
+					<label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
+					<input
+						class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						id="name"
+						type="text"
+						bind:value={playerName}
+						required
+					/>
+				</div>
+				<div class="mb-4">
+					<label class="block text-gray-700 text-sm font-bold mb-2" for="link">
+						Social Media Link (optional)
+					</label>
+					<input
+						class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						id="link"
+						type="text"
+						bind:value={playerLink}
+					/>
+				</div>
+				<div class="flex items-center justify-between">
+					<button
+						class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+						type="submit"
+					>
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+{/if}
